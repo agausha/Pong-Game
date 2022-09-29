78d467464bf31fc7e266cd05a892dab7b837b259
Isaac <uniqucode@gmail.com>
2022-09-29 13:36:58 +0100

    Refrencing gameOverEl

--- js/script.js
+++ js/script.js
@@ -7,3 +7,4 @@ const height = 700;
 const screenWidth = window.screen.width;
 const canvasPosition = screenWidth / 2 - width / 2;
 const isMobile = window.matchMedia('(max-width: 600px)');
+const gameOverEl = document.createElement('div');
